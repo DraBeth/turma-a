7c746fe8a22cd9b757589b1c1d93fd17909b7ac4
DraBeth <cintiaelizabeth@gmail.com>
2026-06-26 20:50:13 -0300

    Add safe useful links data

--- src/data/links.js
+++ src/data/links.js
@@ -1,8 +1,24 @@
 const links = [
-  "LevelUp",
-  "Lyceum",
-  "Manual do Aluno",
-  "Drive da Turma"
+  {
+    titulo: 'Drive da Turma',
+    descricao: 'Materiais, PDFs, gabaritos e arquivos compartilhados. Link direto mantido na descricao do grupo.',
+    url: '#',
+  },
+  {
+    titulo: 'Lyceum',
+    descricao: 'Notas, faltas, boletos, agenda e vida academica oficial.',
+    url: 'https://fmu.lyceum.com.br/AOnline/#/login',
+  },
+  {
+    titulo: 'Level Up',
+    descricao: 'Disciplinas online, atividades EAD e conteudos digitais.',
+    url: 'https://levelup.fmu.br',
+  },
+  {
+    titulo: 'Agenda FMU',
+    descricao: 'Calendario/agenda institucional dentro do Lyceum.',
+    url: 'https://fmu.lyceum.com.br/AOnline/#/home/agenda',
+  },
 ]
 
 export default links
